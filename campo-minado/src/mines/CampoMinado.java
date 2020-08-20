d67a853d500faa874fd7efdd31e580d83f77ce16
Leonardo <lgspezia@yahoo.com.br>
2020-08-20 17:56:45 -0300

    Add files via upload

---
+++ campo-minado/src/mines/CampoMinado.java
@@ -0,0 +1,38 @@
+package com.zetcode;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+@SuppressWarnings("serial")
+public class CampoMinado extends JFrame {
+
+    private JLabel quadro;
+
+    public CampoMinado() {
+        initUI();
+    }
+
+    private void initUI() {
+
+    	quadro = new JLabel("");
+        add(quadro, BorderLayout.SOUTH);
+        add(new Board(quadro));
+        setResizable(false);
+        pack();
+
+        setTitle("Campo Minado");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+        	CampoMinado ex = new CampoMinado();
+            ex.setVisible(true);
+        });
+    }
+}
